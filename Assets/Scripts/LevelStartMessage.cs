cf89c620851d6a7cfe5f75b9e33c72df183b9973
Tati <jaelgonzales18@gmail.com>
2026-05-05 15:57:39 -0400

    organize scripts by folders

--- Assets/Scripts/LevelStartMessage.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LevelStartMessage : MonoBehaviour
-{
-    //void Start()
-    //{
-    //    UIMessageManager.Instance?.ShowMessage("¡Que no te atrapen! Sigue la flecha y busca al Inge Marcelo.", 4f);
-    //}
-}
